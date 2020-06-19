63b3a85d24507547426adbe809fb1a9bb067d50f
Casper Kanaar <casperkanaar@gmail.com>
2020-06-19 17:05:54 +0200

    updated

--- Propeller - Wing/propellervelocities.py
+++ Propeller - Wing/propellervelocities.py
@@ -4,6 +4,6 @@ Created on Fri Jun 12 14:05:37 2020
 
 @author: Casper Kanaar
 
-This program creates a velocity output based on an RPM input
+This program creates a velocity output based on an RPM input. 
 """
 
